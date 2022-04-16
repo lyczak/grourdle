a1db3e42cbcda1ba7b45b23d18f977940fec3d91
cford <kcford26@gmail.com>
2022-04-16 17:03:39 -0400

    check guess working as expected

--- src/wordle.erl
+++ src/wordle.erl
@@ -5,21 +5,54 @@
 %% @doc Check a Wordle guess against a provided word.
 %% @returns A tuple containing the Guess and a list of
 %% colors based on where the letters appear in the provided Word.
+
+%%checks for exact matches, then checks remaining letters for yellows, default grey
+%%3> wordle:check_guess("crate","cheek").
+%%    [green,grey,grey,grey,yellow]
+%%4> wordle:check_guess("check","cheek").
+%%    [green,green,green,grey,green]
+
 check_guess(Guess, Word) ->
-  check_guess_recursion(Guess, Word, Word, []).
 
-check_guess_recursion([], [], FullWord, Result) ->
-  Result;
+  Res = check_for_green(Guess, 1, Word, [grey,grey,grey,grey,grey]),
+  New_guess = element(1, Res),
+  New_word = element(2, Res),
+  New_result = element(3, Res),
+  Final_res = check_for_yellow(New_guess, 1, New_word, New_result),
+  element(3, Final_res).
 
-check_guess_recursion(Guess, Word, FullWord, Result) ->
-  [Ghead | Gtail] = Guess,
-  [Whead | Wtail] = Word,
-  case Ghead =:= Whead of
-    true -> Result = Result ++ green;
-    false -> Result = Result
-  end,
-  case lists:member(Ghead, FullWord) =:= true of
-    true -> Result = Result ++ yellow;
-    false -> Result = Result ++ grey
-  end,
-  check_guess_recursion(Gtail, Wtail, FullWord, Result).
+%lists:nth(index,list)
+check_for_green(Guess, Index, Word, Result) ->
+  Adj_index = (5 - length(Word)) + Index,
+  case Index > length(Word) of
+    true -> {Guess, Word, Result};
+    false ->
+      G_letter = getnth(Index, Guess),
+      W_letter = getnth(Index, Word),
+      case G_letter =:= W_letter of
+               true -> New_result = setnth(Adj_index, Result, green),
+                 check_for_green(lists:delete(G_letter, Guess), Index, lists:delete(W_letter, Word), New_result);
+               false -> check_for_green(Guess, Index + 1, Word, Result)
+             end
+  end.
+
+check_for_yellow(Guess, Index, Word, Result) ->
+  Adj_index = (5 - length(Word)) + Index,
+  case Index > length(Word) of
+    true -> {Guess, Word, Result};
+    false ->
+      G_letter = getnth(Index, Guess),
+      case lists:member(G_letter, Word) of
+               true ->
+                 New_result = setnth(Adj_index, Result, yellow),
+                 check_for_yellow(lists:delete(G_letter, Guess), Index, lists:delete(G_letter, Word), New_result);
+               false -> check_for_yellow(Guess, Index + 1, Word, Result)
+             end
+  end.
+
+%I made these because I'm cringe, dont look at me
+setnth(1, [_|Rest], New) -> [New|Rest];
+setnth(I, [E|Rest], New) -> [E|setnth(I-1, Rest, New)].
+
+getnth(1, [N|_]) -> N;
+getnth(I, [_|Rest]) -> getnth(I-1, Rest).
