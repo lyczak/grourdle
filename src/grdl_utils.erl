dfa0a6b25228f3347d77ca4a33b261e6a28409bb
Del Lyczak <dlyczak@protonmail.com>
2022-04-27 19:32:33 -0400

    add doc to grdl_utils and remove dummy func

--- src/grdl_utils.erl
+++ src/grdl_utils.erl
@@ -1,19 +1,16 @@
 -module(grdl_utils).
 
--export([base64_id/1, unique_base64_id/2, test/0]).
+-export([base64_id/1, unique_base64_id/2]).
 
+% get a cryptographically secure id in base64 that's this many bytes long
 base64_id(Bytes) ->
   base64:encode(crypto:strong_rand_bytes(Bytes)).
 
+% get a cryptographically secure id in base64 that's this many bytes long
+% the id must be a unique key within the provided map
 unique_base64_id(Bytes, Map) ->
   Id = base64_id(Bytes),
   case maps:find(Id, Map) of
     {ok, _} -> unique_base64_id(Bytes, Map);
     error -> Id
-  end.
-
-test() ->
-  receive
-    continue -> test();
-    terminate -> ok
   end.
