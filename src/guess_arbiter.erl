f1ce58c84472cbba04f48ca3b67df64eaef49f1a
jackeselius <ejeselius@gmail.com>
2022-04-28 19:57:13 -0400

    chooses a random guess

--- src/guess_arbiter.erl
+++ src/guess_arbiter.erl
@@ -2,6 +2,11 @@
 
 -export([choose_guess/1]).
 
+
+
 %% @doc Pick/generate a Wordle guess from a list of several guesses.
-choose_guess([First | _]) ->
-  First.
+choose_guess([First | Last]) ->
+  Out = lists:nth(rand:uniform(length([First | Last])), [First | Last]),
+  io:fwrite("~s",[Out]).
+
+
