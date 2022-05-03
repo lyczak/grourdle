befc5fabd9e3fc30903b634426c7e3f49d8882ee
Del Lyczak <dlyczak@protonmail.com>
2022-05-03 19:59:21 -0400

    Add 4-char unique gameids to utils

--- src/grdl_utils.erl
+++ src/grdl_utils.erl
@@ -1,6 +1,6 @@
 -module(grdl_utils).
 
--export([base64_id/1, unique_base64_id/2]).
+-export([base64_id/1, unique_base64_id/2, game_id/1]).
 
 % get a cryptographically secure id in base64 that's this many bytes long
 base64_id(Bytes) ->
@@ -13,4 +13,14 @@ unique_base64_id(Bytes, Map) ->
   case maps:find(Id, Map) of
     {ok, _} -> unique_base64_id(Bytes, Map);
     error -> Id
+  end.
+
+game_id(Map) ->
+  Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  Id = lists:foldl(fun(_, Acc) ->
+    [lists:nth(rand:uniform(length(Chars)), Chars)] ++ Acc
+              end, [], lists:seq(1, 4)),
+  case maps:is_key(Id, Map) of
+    true -> game_id(Map);
+    false -> Id
   end.
