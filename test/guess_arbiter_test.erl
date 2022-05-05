2c7b8ffd4a8415b35ae20c0cc4eef56e3d97de20
jackeselius <ejeselius@gmail.com>
2022-05-05 17:32:02 -0400

    tests for guess_arbiter and wordle

--- test/guess_arbiter_test.erl
+++ test/guess_arbiter_test.erl
@@ -4,6 +4,12 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+get_min_tuple_test()  ->
+  CorrectAnswer = {"table", 4},
+  List = [{"bread", 10}, {"table", 4}, {"movie", 16}],
+  Check = grdl_guess_arbiter:get_min_tuple(List),
+  ?assertEqual(CorrectAnswer, Check).
+
 edit_distance_basic_test() ->
   CorrectAnswer = 4,
   Check = grdl_guess_arbiter:edit_distance("table", "movie"),
@@ -12,6 +18,16 @@ edit_distance_basic_test() ->
 edit_distance_same_two_strings_test() ->
   CorrectAnswer = 0,
   Check = grdl_guess_arbiter:edit_distance("table", "table"),
+  ?assertEqual(CorrectAnswer, Check).
+
+edit_distance_short_first_string_test() ->
+  CorrectAnswer = 2,
+  Check = grdl_guess_arbiter:edit_distance("tab", "table"),
+  ?assertEqual(CorrectAnswer, Check).
+
+edit_distance_short_second_string_test() ->
+  CorrectAnswer = 2,
+  Check = grdl_guess_arbiter:edit_distance("table", "tab"),
   ?assertEqual(CorrectAnswer, Check).
 
 list_to_variable_test() ->
--- test/wordle_test.erl
+++ test/wordle_test.erl
@@ -8,12 +8,12 @@ lowercase1_check_guess_test() ->
   Guess = "stand",
   Word  = "straw",
   Res = grdl_wordle:check_guess(Guess, Word),
-  ?assertMatch({Guess, [green, green, yellow, grey, grey]}, Res).
+  ?assertMatch({Guess, [green, green, yellow, grey, grey]}, {Guess, Res}).
 
 duplicate_letter1_check_guess_test() ->
   Guess = "crass",
   Word  = "sales",
   Res = grdl_wordle:check_guess(Guess, Word),
-  ?assertMatch({Guess, [grey, grey, yellow, yellow, green]}, Res).
+  ?assertMatch({Guess, [grey, grey, yellow, yellow, green]}, {Guess, Res}).
 
 -endif.
