bac6ef6f07489482aca17c1e6bf96516aa40859c
jackeselius <ejeselius@gmail.com>
2022-05-06 16:29:04 -0400

    couple more wordle check guess test conditions

--- test/wordle_test.erl
+++ test/wordle_test.erl
@@ -16,4 +16,22 @@ duplicate_letter1_check_guess_test() ->
   Res = grdl_wordle:check_guess(Guess, Word),
   ?assertMatch({Guess, [grey, grey, yellow, yellow, green]}, {Guess, Res}).
 
+all_green_check_guess_test() ->
+  Guess = "bread",
+  Word  = "bread",
+  Res = grdl_wordle:check_guess(Guess, Word),
+  ?assertMatch({Guess, [green, green, green, green, green]}, {Guess, Res}).
+
+all_grey_check_guess_test() ->
+  Guess = "ggggg",
+  Word  = "bread",
+  Res = grdl_wordle:check_guess(Guess, Word),
+  ?assertMatch({Guess, [grey, grey, grey, grey, grey]}, {Guess, Res}).
+
+all_yellow_check_guess_test() ->
+  Guess = "bread",
+  Word  = "rdaeb",
+  Res = grdl_wordle:check_guess(Guess, Word),
+  ?assertMatch({Guess, [yellow, yellow, yellow, yellow, yellow]}, {Guess, Res}).
+
 -endif.
