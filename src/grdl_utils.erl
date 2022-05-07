3838c77a3cd997eb2e4c8b6eb03927385f974cf2
Del Lyczak <dlyczak@protonmail.com>
2022-05-06 22:42:48 -0400

    Fix incorrect documentation grdl_utils

--- src/grdl_utils.erl
+++ src/grdl_utils.erl
@@ -4,7 +4,7 @@
 
 %% @doc Get a cryptographically secure id in base64 that's this many bytes long.
 %% @param Bytes, the length of the secure id.
-%% @return encode(crypto:strong_rand_bytes(Bytes))., a secure id that is encrypted in base64.
+%% @returns a base64 encoded id
 base64_id(Bytes) ->
   base64:encode(crypto:strong_rand_bytes(Bytes)).
 
@@ -12,7 +12,7 @@ base64_id(Bytes) ->
 %% In addition, the id must be a unique key within the provided map.
 %% @param Bytes, length of id.
 %% @param Map, the Map that the given key is within - also unique within this Map.
-%% @returns either the new Id from base65_id(Bytes) or the unique Id found in the Map parameter.
+%% @returns the unique base64 encoded id
 unique_base64_id(Bytes, Map) ->
   Id = base64_id(Bytes),
   case maps:find(Id, Map) of
@@ -20,9 +20,9 @@ unique_base64_id(Bytes, Map) ->
     error -> Id
   end.
 
-%% @doc Function that returns the Id of the current game or a new game Id if the game is not within the specified Map.
-%% @param Map, the specified Map of games mapped to unique Ids.
-%% @returns Either the Id of a new Game, or the uique Id of the Game within the Map parameter.
+%% @doc generates a new four-character game id unique within the given map
+%% @param Map, the specified Map whose keys are game ids
+%% @returns a new unique four character game id
 game_id(Map) ->
   Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
   Id = lists:foldl(fun(_, Acc) ->
